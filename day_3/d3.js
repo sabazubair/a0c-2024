8b2d337149777dd80cdc9aca59b97565c553fad5
Saba <46948491+sabazubair@users.noreply.github.com>
2024-12-04 23:43:27 -0500

    Added pseudocode for d3, q2

--- day_3/d3.js
+++ day_3/d3.js
@@ -4,24 +4,45 @@ isValidMulInstruction = (instruction) => {
   return /^mul\(\d+,\d+\)$/.test(instruction);
 };
 
+// processAndComputeData = (filePath) => {
+//   let total = 0;
+//   const data = fs.readFileSync(filePath, "utf-8");
+
+//   const lines = data.split("\n"); // Split into lines
+//   for (const line of lines) {
+//     // Extract all valid `mul(x, y)` matches
+//     const matches = line.match(/mul\(\d+,\d+\)/g);
+//     if (matches) {
+//       for (const match of matches) {
+//         if (isValidMulInstruction(match)) {
+//           const [x, y] = match
+//             .match(/\d+/g) // Get all numbers inside `mul`
+//             .map(Number); // Convert them to numbers
+//           total += x * y;
+//         }
+//       }
+//     }
+//   }
+
+//   return total;
+// };
+
 processAndComputeData = (filePath) => {
-  let total = 0;
   const data = fs.readFileSync(filePath, "utf-8");
-
   const lines = data.split("\n"); // Split into lines
-  for (const line of lines) {
-    // Extract all valid `mul(x, y)` matches
-    const matches = line.match(/mul\(\d+,\d+\)/g);
 
-    if (matches) {
-      for (const match of matches) {
-        if (isValidMulInstruction(match)) {
-          const [x, y] = match
-            .match(/\d+/g) // Get all numbers inside `mul`
-            .map(Number); // Convert them to numbers
-          total += x * y;
-        }
-      }
+  let total = 0;
+  let isParsingEnabled = true;
+
+  for (const line of lines) {
+    // add some logic like...
+    if (str === "don't()") {
+      isParsingEnabled = false;
+    } else if (str === "do()") {
+      isParsingEnabled = true;
+    }
+    if (isParsingEnabled && isValidMulInstruction(subStr)) {
+      // do something
     }
   }
 
