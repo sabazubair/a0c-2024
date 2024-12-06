96a760bacbe3b5fca783f60c0ac7d0284c5005e4
Saba <46948491+sabazubair@users.noreply.github.com>
2024-12-05 22:37:36 -0500

    removed redundant isValidInstruction check as it's being completed by regex in line.match

--- day_3/d3.js
+++ day_3/d3.js
@@ -1,32 +1,5 @@
 const fs = require("fs");
 
-isValidMulInstruction = (instruction) => {
-  return /^mul\(\d+,\d+\)$/.test(instruction);
-};
-
-// processAndComputeData = (filePath) => {
-//   let total = 0;
-//   const data = fs.readFileSync(filePath, "utf-8");
-
-//   const lines = data.split("\n"); // Split into lines
-//   for (const line of lines) {
-//     // Extract all valid `mul(x, y)` matches
-//     const matches = line.match(/mul\(\d+,\d+\)/g);
-//     if (matches) {
-//       for (const match of matches) {
-//         if (isValidMulInstruction(match)) {
-//           const [x, y] = match
-//             .match(/\d+/g) // Get all numbers inside `mul`
-//             .map(Number); // Convert them to numbers
-//           total += x * y;
-//         }
-//       }
-//     }
-//   }
-
-//   return total;
-// };
-
 processAndComputeData = (filePath) => {
   const data = fs.readFileSync(filePath, "utf-8");
   const lines = data.split("\n"); // Split into lines
@@ -35,14 +8,17 @@ processAndComputeData = (filePath) => {
   let isParsingEnabled = true;
 
   for (const line of lines) {
-    // add some logic like...
-    if (str === "don't()") {
-      isParsingEnabled = false;
-    } else if (str === "do()") {
-      isParsingEnabled = true;
-    }
-    if (isParsingEnabled && isValidMulInstruction(subStr)) {
-      // do something
+    // Extract all valid `mul(x, y)` matches
+    const matches = line.match(/mul\(\d+,\d+\)/g);
+    if (matches) {
+      for (const match of matches) {
+        if (isValidMulInstruction(match)) {
+          const [x, y] = match
+            .match(/\d+/g) // Get all numbers inside `mul`
+            .map(Number); // Convert them to numbers
+          total += x * y;
+        }
+      }
     }
   }
 
